a56a1d9f7b1906bad2dcefe7253a78306762c184
Anu Jandhyala <anujandhyala@Anu-MacBook-Pro-2.local>
2018-09-05 09:36:28 -0700

    claim detail row

--- node-siebel-rest/siebel_claims_demo/src/Component/Data/ClaimRow.js
+++ node-siebel-rest/siebel_claims_demo/src/Component/Data/ClaimRow.js
@@ -5,13 +5,21 @@ class ClaimRow extends Component {
     const claim = this.props.claim;
 
     return (
+      <table>
+      <tbody>
       <tr>
-        <td>{claim.claimNumber}</td>
-        <td>{claim.reportedDate}</td>
-        <td>{claim.policyNumber}</td>
-        <td>{claim.lastName}</td>
-        <td>{claim.firstName}</td>
+        <td>Claim Number: {claim.claimNumber}</td>
+        <td>Loss Date: {claim.lossDate}</td>
       </tr>
+      <tr>
+        <td>Policy Number: {claim.policyNumber}</td>
+      </tr>
+      <tr>
+        <td>Last Name: {claim.lastName}</td>
+        <td>First Name: {claim.firstName}</td>
+      </tr>
+      </tbody>
+      </table>
     );
   }
 }
