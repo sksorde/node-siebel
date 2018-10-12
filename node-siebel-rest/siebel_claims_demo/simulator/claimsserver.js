d7acbcaac3c643ac8ed9d7cf7114ef676c70cc62
Anu Jandhyala <anujandhyala@Anu-MacBook-Pro-2.local>
2018-10-12 10:17:19 -0700

    remove unused code

--- node-siebel-rest/siebel_claims_demo/simulator/claimsserver.js
+++ node-siebel-rest/siebel_claims_demo/simulator/claimsserver.js
@@ -7,27 +7,6 @@ var fs = require('file-system')
    , certFile = path.resolve(__dirname, 'siebelkeystoreCASigned.cer');
 var siebelUrl = 'https://win-b1ejslvnv0l.siebel-pravici.com:9301';
 
-var CLAIMS = [
-  {"Claim Number": '1-L78XTX', "Policy Number": 'KM-VEH-005', lastName: 'Abdellah', firstName: 'Mohamed', "Status Code": 'OPEN', "Loss Date":'06/12/2018', "Loss Description":"parking lot Thom Thumb store"},
-  {"Claim Number": '1-OOCR13', "Policy Number": 'KM-VEH-005', lastName: 'Abdellah', firstName: 'Mohamed', "Status Code": 'OPEN', "Loss Date":'06/12/2018', "Location Description":"parking lot Thom Thumb store"},
-  {"Claim Number": '1710118', "Policy Number": 'KM-VEH-005', lastName: 'Abdellah', firstName: 'Mohamed', "Status Code": 'OPEN', "Loss Date":'06/12/2018', "Location Description":"parking lot Thom Thumb store"},
-  {"Claim Number": '1710675', "Policy Number": 'KM-VEH-005', lastName: 'Abdellah', firstName: 'Mohamed', "Status Code":'OPEN', "Loss Date":'06/12/2018', "Location Description":"parking lot Thom Thumb store"},
-];
-
-
-var POLICY = [
-  {
-   "Policy Expiration Date":"10/01/2018 06:00:00", "Id":"1-3H01", "Type":"Auto","Primary Last Name": "Abdellah", "Primary First Name": "Mohammed"
-  },
-];
-var DETAILS = [
-  {"Claim Number": '1-L78XTX', "Loss Date":'', "Policy Number": 'KM-VEH-005', lastName: 'Abdellah', firstName: 'Mohammed', "Policy Type": 'AUTO', "Status Code": 'OPEN', "Loss Code":''},
-  {"Claim Number": '1-OOCR13', "Loss Date": '', "Policy Number": '1G1156K01', lastName: 'tester', firstName: 'first2', "Policy Type": 'AUTO', "Status Code": 'OPEN', "Loss Code":''},
-  {"Claim Number": '1710118', "Loss Date": '', "Policy Number": '1K9087TY02', lastName: 'tester', firstName: 'first3', "Policy Type": 'AUTO', "Status Code": 'OPEN', "Loss Code":''},
-  {"Claim Number": '1710675', "Loss Date": '', "Policy Number": 'KM-VEH-006', lastName: 'tester', firstName: 'first1', "Policy Type": 'AUTO', "Status Code": 'OPEN', "Loss Code":''},
-  {"Claim Number": '1711379', "Loss Date": '', "Policy Number": '1A456YU009', lastName: 'tester', firstName: 'first2', "Policy Type": 'AUTO', "Status Code": 'OPEN', "Loss Code":''}
-];
-
 function findDetail(detail) {
   return DETAILS.filter(function(elem) {
 	return detail === elem.claimNumber ? elem : '{}';
